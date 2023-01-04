c3262c2443aec78adc86e2feb095a42eb0ea8a3e
iamcoo <yellow1257tw@gmail.com>
2023-01-04 13:28:27 +0800

    add vnc

--- nano_init.sh
+++ nano_init.sh
@@ -3,12 +3,23 @@
 sudo sh -c 'echo 150 > /sys/devices/pwm-fan/target_pwm'
 
 
-
+#add fan
 cd /etc
 sudo touch rc.local
 
 sudo chmod 777 rc.local
 sudo vim /etc/rc.local
 
+#update
 sudo apt-get update
 sudo apt-get upgrade
+
+#add vnc
+sudo apt install vino
+
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+nmcli connection show
+#dconf write /org/gnome/settings-daemon/plugins/sharing/vino-server/enabled-connections "['UUID of the ethernet']"
+#export DISPLAY=:0
+#/usr/lib/vino/vino-server
